9b7c419b0070fc7c580cece6788748c5ca857e81
Lockdwn20 <lockdwn20@gmail.com>
2025-10-15 14:15:46 -0700

    updated

--- api/thehive-metrics/random.ps1
+++ api/thehive-metrics/random.ps1
@@ -14,3 +14,17 @@ $body = @{
 
 $response = Invoke-RestMethod -Uri "$baseUrl/query" -Method POST -Headers $headers -Body $body
 $response
+
+$body = @{
+    query = @(
+        @{ _name = "listCase" }
+        @{ _name = "page"; from = 0; to = 5 }
+        @{
+            _name   = "project"
+            _fields = @("id","title","status","createdAt","caseTemplate")
+        }
+    )
+} | ConvertTo-Json -Depth 10 -Compress
+
+$response = Invoke-RestMethod -Uri "$baseUrl/query" -Method POST -Headers $headers -Body $body
+$response
