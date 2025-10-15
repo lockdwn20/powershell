402401377383eb15b73e49aec1433ba888b92edd
Lockdwn20 <lockdwn20@gmail.com>
2025-10-15 11:36:59 -0700

    updated

--- api/thehive-metrics/random.ps1
+++ api/thehive-metrics/random.ps1
@@ -9,3 +9,17 @@ try {
         Write-Host "Response body: $($reader.ReadToEnd())"
     }
 }
+
+
+$body = @{
+    query = @(
+        @{ _name = "listCase"; size = 1 }
+    )
+} | ConvertTo-Json -Depth 10
+
+try {
+    $test = Invoke-RestMethod -Uri "$baseUrl/query" -Method POST -Headers $headers -Body $body -TimeoutSec 15
+    $test | ConvertTo-Json -Depth 5
+} catch {
+    Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
+}
